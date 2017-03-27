50e93d7efbbcadf6f7a53440e27b4deda424e85b
jessehartloff <hartloff@buffalo.edu>
2017-03-27 15:22:54 -0400

    initial commit

---
+++ src/PS8_Libraries.java
@@ -0,0 +1,126 @@
+import java.util.ArrayList;
+
+import cse115.math.NormalDistribution;
+import cse115.math.Polynomial;
+import cse115.words.Words;
+
+public class PS8_Libraries{
+
+
+    static double Q1(NormalDistribution distro, double x){
+        // return the z-score of x in the given distribution
+
+        return 0.0;
+    }
+
+
+    static Polynomial Q2(){
+        // return a new Polynomial representing "8x^2 + 5x - 6"
+
+        return null;
+    }
+
+
+    static Polynomial Q3(ArrayList<Double> coefficients){
+        // return a new Polynomial with the provided coefficients
+
+        return null;
+    }
+
+
+    static double Q4(Polynomial equation, double x){
+        // return the evaluation of the input polynomial at x
+
+        return 0.0;
+    }
+
+
+    static Polynomial Q5(Polynomial equation){
+        // return the derivative of the input polynomial
+
+        return null;
+    }
+
+
+
+    static int Q6(String word){
+        // return the number of syllable in the input word
+
+        return 0;
+    }
+
+
+    static boolean Q7(String word1, String word2){
+        // return true if the two input words rhyme at least 1 syllable, false otherwise
+        //
+        // Words rhyme if they end with the same syllable
+
+        return false;
+    }
+
+
+    static boolean Q8(String word1, String word2, String word3){
+        // return true if the three input words all rhyme at least 1 syllable, false otherwise
+
+        return false;
+    }
+
+
+    static boolean Q9(String word1, String word2){
+        // return true if the two input words alliterate with at least 1 sound, false otherwise.
+        //
+        // Words alliterate if they start with the same sound
+
+        return false;
+    }
+
+
+    static boolean Q10(String word1, String word2, String word3){
+        // return true if the three input words all alliterate with at least 1 sound, false otherwise
+
+        return false;
+    }
+
+
+    static int Q11(String sentence){
+        // return the number of syllables in the given sentence. The input is sentence with each word separated by
+        // spaces and no punctuation.
+
+        return 0;
+    }
+
+
+    static ArrayList<String> Q12(String word){
+        // return all possible parts of speech of the given word
+
+
+        return null;
+    }
+
+
+    static int Q13(String sentence, String partOfSpeech){
+        // return the maximum possible number of words in the input sentence that can have the given part of speech.
+
+        return 0;
+    }
+
+
+    static boolean Q14(String sentence1, String sentence2){
+        // return true if the two input sentences rhyme at one syllable. The sentences rhyme if their last words rhyme.
+
+        return false;
+    }
+
+
+    static boolean Q15(ArrayList<String> words){
+        // return true if all the words in the input list alliterate with at least 1 sound, false otherwise
+
+        return false;
+    }
+
+
+    public static void main(String[] args){
+
+    }
+
+}
